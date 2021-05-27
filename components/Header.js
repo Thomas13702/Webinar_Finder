d6066fa167a127a2dc5c7dc95236f4f9d48e175c
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-27 14:53:25 +0100

    Intital Commit - Set up environment and added a Header navbar

---
+++ components/Header.js
@@ -0,0 +1,72 @@
+import { FaBars, FaTimes, FaRegDotCircle } from "react-icons/fa";
+import { SiWebpack } from "react-icons/si";
+import { useState, useEffect } from "react"; //have to bring in when you want to use a specific context
+import Link from "next/link";
+import styles from "@/styles/Header.module.css";
+
+export default function Header() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <div className={styles.header}>
+      <div className={styles.logoNav}>
+        <div className={styles.logoContainer}>
+          <Link href="/">
+            <a className={styles.logo}>
+              {/* <FaRegDotCircle  /> */}
+              Webinar Finder
+            </a>
+          </Link>
+        </div>
+
+        <ul
+          className={
+            click ? `${styles.navOptions} ${styles.active}` : styles.navOptions
+          }
+        >
+          <li className={styles.option} onClick={closeMobileMenu}>
+            <a href="#">Webinars</a>
+          </li>
+          <li className={styles.option} onClick={closeMobileMenu}>
+            <a href="#">Add Events</a>
+          </li>
+          <li className={styles.option} onClick={closeMobileMenu}>
+            <a href="#">Dashboard</a>
+          </li>
+          <li
+            className={`${styles.option} ${styles.mobileOption}`}
+            onClick={closeMobileMenu}
+          >
+            <a href="#">SIGN-IN</a>
+          </li>
+          <li
+            className={`${styles.option} ${styles.mobileOption}`}
+            onClick={closeMobileMenu}
+          >
+            <a href="" className={styles.signUp}>
+              SIGN-UP
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul className={styles.signinUp}>
+        <li className={styles.signIn} onClick={closeMobileMenu}>
+          <a href="#">SIGN-IN</a>
+        </li>
+        <li onClick={closeMobileMenu}>
+          <a href="" className={styles.signupBtn}>
+            SIGN-UP
+          </a>
+        </li>
+      </ul>
+      <div className={styles.mobileMenu} onClick={handleClick}>
+        {click ? (
+          <FaTimes className={styles.menuIcon} />
+        ) : (
+          <FaBars className={styles.menuIcon} />
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ components/Layout.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Header from "./Header";
+import Footer from "./Footer";
+// import Showcase from "./Showcase";
+import styles from "@/styles/Layout.module.css";
+
+export default function Layout({ title, keywords, description, children }) {
+  //layout will wrap content, what ever is wrapped will be the children
+
+  const router = useRouter();
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      <Header />
+      {/* {router.pathname === "/" && <Showcase />} */}
+      {/* checks to see if we are on the home page */}
+      <div className={styles.container}>{children}</div>
+      <Footer />
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "DJ Events | Find the hottest parties",
+  description: "Find the latest Dj and other musical events",
+  keywords: "music, dj, edm, events",
+};
