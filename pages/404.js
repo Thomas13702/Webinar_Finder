4a131b5b19101383e398084c0f19fb1aa67ea5ee
Thomas13702 <thomaspritchard137@btinternet.com>
2021-05-30 18:06:09 +0100

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Layout from "@/components/Layout";
+import styles from "@/styles/404.module.css";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <div className={styles.imageBackground}>
+        <h1 className={styles.text}>404</h1>
+        <p className={styles.p}>There is nothing here</p>
+        <div className={styles.linkDiv}>
+          <Link href="/">
+            <a className={styles.link}>Go Home</a>
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+}
