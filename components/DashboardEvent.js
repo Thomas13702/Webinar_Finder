732ffa3ed83ee62384714b678bf015eca24fa664
Thomas13702 <thomaspritchard137@btinternet.com>
2021-06-13 18:12:10 +0100

    Display Dashboard webinars

---
+++ components/DashboardEvent.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import styles from "@/styles/DashboardEvent.module.css";
+
+export default function DashboardEvent({ webs, handleDelete }) {
+  return (
+    <div className={styles.event}>
+      <h4>
+        <Link href={`/webinars/${webs.slug}`}>
+          <a>{webs.name}</a>
+        </Link>
+      </h4>
+      <Link href={`/webinars/edit/${webs.id}`}>
+        <a className={styles.edit}>
+          <FaPencilAlt /> <span>Edit Event</span>
+        </a>
+      </Link>
+      <a className={styles.delete} onClick={() => handleDelete(webs.id)}>
+        <FaTimes /> <span>Delete Event</span>
+      </a>
+    </div>
+  );
+}
--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,12 +1,27 @@
 import Layout from "@/components/Layout";
+import DashboardEvent from "@/components/DashboardEvent";
 import { parseCookies } from "@/helpers/index";
 import { API_URL } from "@/config/index";
+import styles from "@/styles/DashBoard.module.css";
 
 export default function DashboardPage({ webinars }) {
-  console.log(webinars);
+  const deleteEvent = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Webinars</h3>
+        {webinars.map((webs) => (
+          <DashboardEvent
+            key={webs.id}
+            webs={webs}
+            handleDelete={deleteEvent}
+          />
+        ))}
+      </div>
     </Layout>
   );
 }
